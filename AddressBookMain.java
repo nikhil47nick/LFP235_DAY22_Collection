ae8f3934381af7b2dbdff8c579824059c33994e5
nikhil47nick <nikhiljoji94@gmail.com>
2023-01-15 14:30:38 +0530

    Added :Prgram

---
+++ AddressBookMain.java
@@ -0,0 +1,7 @@
+package Com.BridgeLabz.Collection;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Program");
+    }
+}
